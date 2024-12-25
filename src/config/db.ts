6a1fc86af2de2810cc6fb78dc86488d9a62f44b9
himalf <himal.fullel15@gmail.com>
2024-12-25 14:45:12 +0545

    fix: fix db error with category

--- src/config/db.ts
+++ src/config/db.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined,
 });
 pool.getConnection((err, connection) => {
