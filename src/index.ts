db24d54f241a5a38799cdb3aded25fa8549a8612
himalf <himal.fullel15@gmail.com>
2024-12-25 20:50:08 +0545

    feat: make api call to route for product

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-// src/index.ts
+// Import section
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import bodyparser from "body-parser";
+import Category from "./route/category";
+import Product from "./route/product";
 dotenv.config();
-
+// middlewares
 const app: Express = express();
 app.use(express.json());
 app.use(bodyparser.json());
 const port = process.env.PORT || 3000;
-import Category from "./route/category";
-import bodyParser from "body-parser";
+
+app.use("/products", Product);
 app.use("/categories", Category);
 app.get("/", (req: Request, res: Response) => {
   res.send("Express + TypeScript Server");
 });
-
+// listening to the port or running the server
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
